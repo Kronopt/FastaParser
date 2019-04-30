bc8aeefb5d2c8b3fdc6b396ee0a82902532a7241
Kronopt <Kronopt@users.noreply.github.com>
2019-04-30 13:04:41 +0100

    Added parse_method parameter to FastaParser
    - FastaParser
    - - parse_method parameter and logic
    
    - LetterCode
    - - Fixed docstrings
    - - Changed ValueError to TypeError for wrong parameter str option of sequence_type
    
    - FastaSequence
    - - Fixed/added docstrings
    - - Changed logic for identifying if last sequence was parsed or not

--- FastaParser/FastaParser.py
+++ FastaParser/FastaParser.py
@@ -23,6 +23,7 @@ https://ncbi.github.io/cxx-toolkit/pages/ch_demo#ch_demo.id1_fetch.html_ref_fast
 
 
 import warnings
+from collections import namedtuple
 
 
 __author__ = 'Pedro HC David, https://github.com/Kronopt'
@@ -121,9 +122,7 @@ class LetterCode:
     Raises
     ------
     TypeError
-        If letter_code is not str.
-    ValueError
-        If sequence_type is not 'nucleotide', 'aminoacid' or None.
+        If letter_code or sequence_type are of the wrong type.
     """
     _letter_code_dictionary = {
         'nucleotide': (nucleotide_letter_codes_good, nucleotide_letter_codes_degenerate),
@@ -138,15 +137,13 @@ class LetterCode:
         ----------
         letter_code : str
             Letter code.
-        sequence_type : str or None, optional
-            'nucleotide' or 'aminoacid.
+        sequence_type : 'nucleotide', 'aminoacid' or None, optional
+            'nucleotide' or 'aminoacid' type sequence, None if there is no information.
 
         Raises
         ------
         TypeError
-            If letter_code is not str.
-        ValueError
-            If sequence_type is not 'nucleotide', 'aminoacid' or None.
+            If letter_code or sequence_type are of the wrong type.
         """
         if isinstance(letter_code, str):
             self._letter_code = letter_code.upper()
@@ -175,7 +172,7 @@ class LetterCode:
         elif self._sequence_type is None:
             self._supported = False
         else:
-            raise ValueError('sequence_type, if defined, must be one of: %s' % ', '.join(self._letter_code_dictionary))
+            raise TypeError('sequence_type, if defined, must be one of: %s' % ', '.join(self._letter_code_dictionary))
 
     @property
     def letter_code(self):
@@ -214,7 +211,7 @@ class FastaSequence:
         Description portion of the definition line (header). Can be empty.
     sequence : list of LetterCode
         Sequence.
-    sequence_type : str or None
+    sequence_type : 'nucleotide', 'aminoacid' or None
         Indicates the type of sequence ('aminoacid' or 'nucleotide'). Can be None if not known.
     inferred_type: bool
         True if FastaSequence inferred the sequence type, False otherwise.
@@ -235,7 +232,7 @@ class FastaSequence:
             FASTA sequence definition line (header), containing the '>' symbol at the start.
         sequence : str
             String of characters representing a DNA, RNA or aminoacid sequence.
-        sequence_type : str or None, optional
+        sequence_type : 'nucleotide', 'aminoacid' or None, optional
             Indicates the type of sequence ('aminoacid' or 'nucleotide').
             If not defined, FastaSequence can try to infer type based on the letter codes.
         infer_type : bool, optional
@@ -268,7 +265,7 @@ class FastaSequence:
                 or sequence_type is None:
             self._sequence_type = sequence_type
         else:
-            raise TypeError('sequence_type must be str or None')
+            raise TypeError('sequence_type must be one of: %s or None' % LetterCode._letter_code_dictionary)
 
         if isinstance(sequence, str):
             if isinstance(infer_type, bool):
@@ -385,23 +382,29 @@ class FastaSequence:
 class FastaParser:
     """
     Parses the given FASTA file.
+    Parsing methods include 'quick' or 'rich':
+        - 'quick': Object containing just the FASTA header and sequence attributes
+        - 'rich': FastaSequence (default)
 
     Attributes
     ----------
     fasta_file : FASTA file object
         The FASTA file passed as parameter.
-    sequences_type : str or None
+    sequences_type : 'nucleotide', 'aminoacid' or None
         Indicates the type of sequences to expect ('aminoacid' or 'nucleotide'). Can be None if not known.
     infer_type: bool
         True if sequence type of each sequence is to be inferred, False otherwise.
+    parse_method: 'rich' or 'quick'
+        Parse method used ('rich' or 'quick').
 
     Raises
     ------
     TypeError
+        If fasta_file_object, sequences_type, infer_type or parse_method are of the wrong type.
         If fasta_file_object is not a file object or is closed.
     """
 
-    def __init__(self, fasta_file_object, sequences_type=None, infer_type=False):
+    def __init__(self, fasta_file_object, sequences_type=None, infer_type=False, parse_method='rich'):
         """
         Initializes file object (checks if fasta_file_object is an opened file object).
         Only one FASTA sequence is read at a time from the FASTA file, previous sequences are not kept in memory.
@@ -410,17 +413,27 @@ class FastaParser:
         ----------
         fasta_file_object : file object
             An opened file handle.
-        sequences_type : str or None, optional
-            Indicates the type of sequences to expect ('aminoacid' or 'nucleotide').
+        sequences_type : 'nucleotide', 'aminoacid' or None, optional
+            Indicates the type of sequences to expect ('aminoacid' or 'nucleotide'). None if unknown.
         infer_type : bool, optional
             Indicates if FastaParser should try to infer aminoacid sequence type for each sequence.
             Can only identify aminoacid sequences.
+        parse_method: 'rich' or 'quick', optional
+            Parse method to use ('rich' or 'quick'). Defaults to 'rich'.
+            'quick' parsing method just parses the header and the sequence into individual properties,
+            so it's much faster and less memory intensive. If selected, sequences_type and
+            infer_type parameters are ignored
+            'rich' implements more functionality, but is a bit slower.
 
         Raises
         ------
         TypeError
+            If fasta_file_object, sequences_type, infer_type or parse_method are of the wrong type.
             If fasta_file_object is not a file object or is closed.
         """
+        # for 'quick' parse method
+        self._fasta_sequence = namedtuple('Fasta', ['header', 'sequence'])
+
         if hasattr(fasta_file_object, "readline"):  # assume it's a file object
             if fasta_file_object.closed:
                 raise TypeError('fasta_file_object must be opened for reading')
@@ -433,12 +446,18 @@ class FastaParser:
                 or sequences_type is None:
             self._sequences_type = sequences_type
         else:
-            raise TypeError('sequences_type must be str or None')
+            raise TypeError('sequence_type must be one of: %s or None' % LetterCode._letter_code_dictionary)
 
         if isinstance(infer_type, bool):
             self._infer_type = infer_type
         else:
             raise TypeError('infer_type must be bool')
+
+        parse_methods = ('rich', 'quick')
+        if isinstance(parse_method, str) and parse_method in parse_methods:
+            self._parse_method = parse_method
+        else:
+            raise TypeError('parse_method must be one of: %s' % ', '.join(parse_methods))
 
     @property
     def fasta_file(self):
@@ -451,6 +470,10 @@ class FastaParser:
     @property
     def infer_type(self):
         return self._infer_type
+
+    @property
+    def parse_method(self):
+        return self._parse_method
 
     def __iter__(self):
         """
@@ -480,7 +503,12 @@ class FastaParser:
                     if len(line) > 0 and line[0] != '>':
                         sequence += line
                     elif len(line) > 0 and line[0] == '>':
-                        yield FastaSequence(definition_line, sequence, self._sequences_type, self._infer_type)
+                        if self._parse_method == 'rich':
+                            fasta_sequence = FastaSequence(definition_line, sequence, self._sequences_type,
+                                                           self._infer_type)
+                        else:  # 'quick'
+                            fasta_sequence = self._fasta_sequence(definition_line, sequence)
+                        yield fasta_sequence
 
                         # restart variables
                         definition_line = line
@@ -488,8 +516,12 @@ class FastaParser:
                         parsing_fasta_sequence = True
 
             # end of file, therefore yield last FASTA sequence
-            if len(definition_line) > 0:
-                yield FastaSequence(definition_line, sequence, self._sequences_type, self._infer_type)
+            if len(sequence) > 0:  # a FASTA sequence was actually parsed and were not just blank lines
+                if self._parse_method == 'rich':
+                    fasta_sequence = FastaSequence(definition_line, sequence, self._sequences_type, self._infer_type)
+                else:  # 'quick'
+                    fasta_sequence = self._fasta_sequence(definition_line, sequence)
+                yield fasta_sequence
 
         return iter_fasta_file(self._fasta_file)
 
