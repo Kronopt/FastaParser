e89d879ca0b518267517d125fd8363410922439d
Kronopt <Kronopt@users.noreply.github.com>
2020-01-21 20:29:37 +0000

    Tests: Reader

--- tests/test_Reader.py
+++ tests/test_Reader.py
@@ -15,39 +15,116 @@ from FastaParser import Reader
 ##########
 
 
+def fasta_contents(fasta_path):
+    with open(fasta_path) as f:
+        fastas = []
+        _id = ''
+        fasta = ''
+        start_of_fasta = True
+        for line in f:
+            if line.startswith('>') and start_of_fasta:
+                start_of_fasta = False
+                _id, description = line.rstrip()[1:].split(maxsplit=1)
+            elif line.startswith('>'):
+                fastas.append((_id, description, fasta))
+                _id, description = line.rstrip()[1:].split(maxsplit=1)
+                fasta = ''
+            else:
+                fasta += line.rstrip()
+        # last fasta
+        fastas.append((_id, description, fasta))
+    return fastas  # (id, description, fasta)
+
+
 @pytest.fixture()
 def fasta_empty():
-    f = open('tests/fasta_empty.fasta', 'r')
+    f = open('tests/fasta_empty.fasta')
     yield f
     f.close()
+
+
+@pytest.fixture()
+def fasta_no_empty_lines():
+    f = open('tests/fasta_no_empty_lines.fasta')
+    yield f
+    f.close()
+
+
+@pytest.fixture()
+def fasta_no_empty_lines_contents():
+    return fasta_contents('tests/fasta_no_empty_lines.fasta')
+
+
+@pytest.fixture()
+def fasta_multiple_empty_lines():
+    f = open('tests/fasta_multiple_empty_lines.fasta')
+    yield f
+    f.close()
+
+
+@pytest.fixture()
+def fasta_multiple_empty_lines_contents():
+    return fasta_contents('tests/fasta_multiple_empty_lines.fasta')
+
+
+@pytest.fixture()
+def fasta_empty_lines_in_sequence():
+    f = open('tests/fasta_empty_lines_in_sequence.fasta')
+    yield f
+    f.close()
+
+
+@pytest.fixture()
+def fasta_empty_lines_in_sequence_contents():
+    return fasta_contents('tests/fasta_empty_lines_in_sequence.fasta')
 
 
 @pytest.fixture()
 def fasta_nucleotide_single():
-    f = open('tests/fasta_nucleotide_single.fasta', 'r')
+    f = open('tests/fasta_nucleotide_single.fasta')
     yield f
     f.close()
+
+
+@pytest.fixture()
+def fasta_nucleotide_single_contents():
+    return fasta_contents('tests/fasta_nucleotide_single.fasta')
 
 
 @pytest.fixture()
 def fasta_nucleotide_multiple():
-    f = open('tests/fasta_nucleotide_multiple.fasta', 'r')
+    f = open('tests/fasta_nucleotide_multiple.fasta')
     yield f
     f.close()
+
+
+@pytest.fixture()
+def fasta_nucleotide_multiple_contents():
+    return fasta_contents('tests/fasta_nucleotide_multiple.fasta')
 
 
 @pytest.fixture()
 def fasta_aminoacid_single():
-    f = open('tests/fasta_aminoacid_single.fasta', 'r')
+    f = open('tests/fasta_aminoacid_single.fasta')
     yield f
     f.close()
 
 
 @pytest.fixture()
+def fasta_aminoacid_single_contents():
+    return fasta_contents('tests/fasta_aminoacid_single.fasta')
+
+
+@pytest.fixture()
 def fasta_aminoacid_multiple():
-    f = open('tests/fasta_aminoacid_multiple.fasta', 'r')
+    f = open('tests/fasta_aminoacid_multiple.fasta')
     yield f
     f.close()
+
+
+@pytest.fixture()
+def fasta_aminoacid_multiple_contents():
+    return fasta_contents('tests/fasta_aminoacid_multiple.fasta')
 
 
 #######
@@ -148,7 +225,157 @@ class Test__init__:
 
 
 class Test__iter__:
-    pass
+    def test_closed_file(self, fasta_empty):
+        fasta_reader = Reader(fasta_empty)
+        fasta_empty.close()
+        with pytest.raises(TypeError):
+            fasta_reader.__iter__()
+
+    def test_current_iterator(self, fasta_empty):
+        fasta_reader = Reader(fasta_empty)
+        assert not hasattr(fasta_reader, '_current_iterator')
+        fasta_reader.__iter__()
+        assert hasattr(fasta_reader, '_current_iterator')
+        try:  # test if is iterable
+            iter(fasta_reader._current_iterator)
+        except TypeError:
+            pytest.fail('Reader._current_iterator is not iterable.')
+
+    def test_empty_fasta_file_rich(self, fasta_empty):
+        fasta_reader = Reader(fasta_empty)
+        fastas = []
+        for fasta in fasta_reader:
+            fastas.append(fasta)
+        assert len(fastas) == 0
+
+    def test_empty_fasta_file_quick(self, fasta_empty):
+        fasta_reader = Reader(fasta_empty, parse_method='quick')
+        fastas = []
+        for fasta in fasta_reader:
+            fastas.append(fasta)
+        assert len(fastas) == 0
+
+    def test_single_fasta_file_rich(self, fasta_nucleotide_single, fasta_nucleotide_single_contents,
+                                    fasta_aminoacid_single, fasta_aminoacid_single_contents):
+        # nucleotide
+        fasta_nucleotide_reader = Reader(fasta_nucleotide_single, sequences_type='nucleotide')
+        fastas_nucleotide = []
+        for fasta in fasta_nucleotide_reader:
+            fastas_nucleotide.append(fasta)
+        assert len(fastas_nucleotide) == 1
+        assert fastas_nucleotide[0].sequence_as_string() == fasta_nucleotide_single_contents[0][2]
+        assert fastas_nucleotide[0].id == fasta_nucleotide_single_contents[0][0]
+        assert fastas_nucleotide[0].description == fasta_nucleotide_single_contents[0][1]
+        assert fastas_nucleotide[0].sequence_type == 'nucleotide'
+        assert fastas_nucleotide[0].inferred_type is False
+        # aminoacid
+        fasta_aminoacid_reader = Reader(fasta_aminoacid_single, sequences_type='aminoacid')
+        fastas_aminoacid = []
+        for fasta in fasta_aminoacid_reader:
+            fastas_aminoacid.append(fasta)
+        assert len(fastas_aminoacid) == 1
+        assert fastas_aminoacid[0].sequence_as_string() == fasta_aminoacid_single_contents[0][2]
+        assert fastas_aminoacid[0].id == fasta_aminoacid_single_contents[0][0]
+        assert fastas_aminoacid[0].description == fasta_aminoacid_single_contents[0][1]
+        assert fastas_aminoacid[0].sequence_type == 'aminoacid'
+        assert fastas_aminoacid[0].inferred_type is False
+
+    def test_single_fasta_file_quick(self, fasta_nucleotide_single, fasta_nucleotide_single_contents,
+                                     fasta_aminoacid_single, fasta_aminoacid_single_contents):
+        # nucleotide
+        fasta_nucleotide_reader = Reader(fasta_nucleotide_single, parse_method='quick')
+        fastas_nucleotide = []
+        for fasta in fasta_nucleotide_reader:
+            fastas_nucleotide.append(fasta)
+        assert len(fastas_nucleotide) == 1
+        assert fastas_nucleotide[0].sequence == fasta_nucleotide_single_contents[0][2]
+        assert fastas_nucleotide[0].header == '>' + ' '.join((fasta_nucleotide_single_contents[0][0],
+                                                              fasta_nucleotide_single_contents[0][1]))
+        # aminoacid
+        fasta_aminoacid_reader = Reader(fasta_aminoacid_single, parse_method='quick')
+        fastas_aminoacid = []
+        for fasta in fasta_aminoacid_reader:
+            fastas_aminoacid.append(fasta)
+        assert len(fastas_aminoacid) == 1
+        assert fastas_aminoacid[0].sequence == fasta_aminoacid_single_contents[0][2]
+        assert fastas_aminoacid[0].header == '>' + ' '.join((fasta_aminoacid_single_contents[0][0],
+                                                             fasta_aminoacid_single_contents[0][1]))
+
+    def test_multiple_fasta_file_rich(self, fasta_nucleotide_multiple, fasta_nucleotide_multiple_contents,
+                                      fasta_aminoacid_multiple, fasta_aminoacid_multiple_contents):
+        # nucleotide
+        fasta_nucleotide_reader = Reader(fasta_nucleotide_multiple, sequences_type='nucleotide')
+        fastas_nucleotide = []
+        for fasta in fasta_nucleotide_reader:
+            fastas_nucleotide.append(fasta)
+            assert fasta.sequence_as_string() == fasta_nucleotide_multiple_contents[len(fastas_nucleotide)-1][2]
+            assert fasta.id == fasta_nucleotide_multiple_contents[len(fastas_nucleotide)-1][0]
+            assert fasta.description == fasta_nucleotide_multiple_contents[len(fastas_nucleotide)-1][1]
+            assert fasta.sequence_type == 'nucleotide'
+            assert fasta.inferred_type is False
+        assert len(fastas_nucleotide) == 17
+        # aminoacid
+        fasta_aminoacid_reader = Reader(fasta_aminoacid_multiple, sequences_type='aminoacid')
+        fastas_aminoacid = []
+        for fasta in fasta_aminoacid_reader:
+            fastas_aminoacid.append(fasta)
+            assert fasta.sequence_as_string() == fasta_aminoacid_multiple_contents[len(fastas_aminoacid)-1][2]
+            assert fasta.id == fasta_aminoacid_multiple_contents[len(fastas_aminoacid)-1][0]
+            assert fasta.description == fasta_aminoacid_multiple_contents[len(fastas_aminoacid)-1][1]
+            assert fasta.sequence_type == 'aminoacid'
+            assert fasta.inferred_type is False
+        assert len(fastas_aminoacid) == 20
+
+    def test_multiple_fasta_file_quick(self, fasta_nucleotide_multiple, fasta_nucleotide_multiple_contents,
+                                       fasta_aminoacid_multiple, fasta_aminoacid_multiple_contents):
+        # nucleotide
+        fasta_nucleotide_reader = Reader(fasta_nucleotide_multiple, parse_method='quick')
+        fastas_nucleotide = []
+        for fasta in fasta_nucleotide_reader:
+            fastas_nucleotide.append(fasta)
+            assert fasta.sequence == fasta_nucleotide_multiple_contents[len(fastas_nucleotide)-1][2]
+            assert fasta.header == '>' + ' '.join((fasta_nucleotide_multiple_contents[len(fastas_nucleotide)-1][0],
+                                                   fasta_nucleotide_multiple_contents[len(fastas_nucleotide)-1][1]))
+        assert len(fastas_nucleotide) == 17
+        # aminoacid
+        fasta_aminoacid_reader = Reader(fasta_aminoacid_multiple, parse_method='quick')
+        fastas_aminoacid = []
+        for fasta in fasta_aminoacid_reader:
+            fastas_aminoacid.append(fasta)
+            assert fasta.sequence == fasta_aminoacid_multiple_contents[len(fastas_aminoacid)-1][2]
+            assert fasta.header == '>' + ' '.join((fasta_aminoacid_multiple_contents[len(fastas_aminoacid)-1][0],
+                                                   fasta_aminoacid_multiple_contents[len(fastas_aminoacid)-1][1]))
+        assert len(fastas_aminoacid) == 20
+
+    def test_empty_lines_between_fastas(self, fasta_multiple_empty_lines, fasta_multiple_empty_lines_contents):
+        fasta_reader = Reader(fasta_multiple_empty_lines, parse_method='quick')
+        fastas = []
+        for fasta in fasta_reader:
+            fastas.append(fasta)
+            assert fasta.sequence == fasta_multiple_empty_lines_contents[len(fastas)-1][2]
+            assert fasta.header == '>' + ' '.join((fasta_multiple_empty_lines_contents[len(fastas)-1][0],
+                                                   fasta_multiple_empty_lines_contents[len(fastas)-1][1]))
+        assert len(fastas) == 2
+
+    def test_fasta_no_empty_lines(self, fasta_no_empty_lines, fasta_no_empty_lines_contents):
+        fasta_reader = Reader(fasta_no_empty_lines, parse_method='quick')
+        fastas = []
+        for fasta in fasta_reader:
+            fastas.append(fasta)
+            assert fasta.sequence == fasta_no_empty_lines_contents[len(fastas)-1][2]
+            assert fasta.header == '>' + ' '.join((fasta_no_empty_lines_contents[len(fastas)-1][0],
+                                                   fasta_no_empty_lines_contents[len(fastas)-1][1]))
+        assert len(fastas) == 2
+
+    def test_fasta_empty_lines_in_sequence(self, fasta_empty_lines_in_sequence, fasta_empty_lines_in_sequence_contents):
+        fasta_reader = Reader(fasta_empty_lines_in_sequence, parse_method='quick')
+        fastas = []
+        for fasta in fasta_reader:
+            fastas.append(fasta)
+            assert fasta.sequence == fasta_empty_lines_in_sequence_contents[len(fastas) - 1][2]
+            assert fasta.header == '>' + ' '.join((fasta_empty_lines_in_sequence_contents[len(fastas) - 1][0],
+                                                   fasta_empty_lines_in_sequence_contents[len(fastas) - 1][1]))
+        assert len(fastas) == 2
 
 
 class Test__next__:
@@ -164,20 +391,3 @@ class Test__repr__:
 #   class Test_sequences_type
 #   class Test_infer_type_property
 #   class Test_parse_method_property
-
-
-# TODO
-# empty file
-# single fasta file
-# multiple fasta file
-
-# quick and rich parsing methods
-# nucleotide and aminoacid sequences
-
-# empty lines at the end of file
-# empty lines at the beginning of file
-# no empty lines at the end of file
-# empty lines between fastas in multiple fasta file
-# empty lines in the middle of a sequence
-# headers one after the other
-# characters not in fasta specification
