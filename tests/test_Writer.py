71cbe8462dab474d578cfb74b7d9ace4d2501c24
Kronopt <Kronopt@users.noreply.github.com>
2020-01-22 16:43:01 +0000

    Tests: Writer

---
+++ tests/test_Writer.py
@@ -0,0 +1,28 @@
+#!python
+# coding: utf-8
+
+"""
+Tests for FastaParser.Writer class
+"""
+
+
+import pytest
+from FastaParser import Writer
+
+
+##########
+# Fixtures
+##########
+
+
+#######
+# Tests
+#######
+
+
+# TODO
+# class Test__init__:
+# class Test_fasta_file_property:
+# class Test_writefasta:
+# class Test_writefastas:
+# class Test__repr__:
