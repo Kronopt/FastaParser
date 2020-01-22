6957626c0c171453a2831313b0c46a8b5ec4cef0
Kronopt <Kronopt@users.noreply.github.com>
2020-01-22 17:37:09 +0000

    Tests: Writer

--- tests/test_Writer.py
+++ tests/test_Writer.py
@@ -6,6 +6,7 @@ Tests for FastaParser.Writer class
 """
 
 
+import os
 import pytest
 from FastaParser import Writer
 
@@ -15,14 +16,47 @@ from FastaParser import Writer
 ##########
 
 
+@pytest.fixture()
+def fasta_temporary_file():
+    f = open('tests/FASTA_TEMPORARY_FILE_FOR_WRITING.fasta', 'w')
+    yield f
+    f.close()
+    os.remove(f.name)
+
+
 #######
 # Tests
 #######
 
 
+class Test__init__:
+    def test_fasta_file_object_good(self, fasta_temporary_file):
+        fasta_writer = Writer(fasta_temporary_file)
+        assert fasta_writer.fasta_file is fasta_temporary_file
+
+    def test_fasta_file_object_closed(self, fasta_temporary_file):
+        fasta_temporary_file.close()
+        with pytest.raises(TypeError):
+            Writer(fasta_temporary_file)
+
+    def test_fasta_file_object_not_a_file(self):
+        with pytest.raises(TypeError):
+            Writer('')
+        with pytest.raises(TypeError):
+            Writer([])
+        with pytest.raises(TypeError):
+            Writer(123)
+
+
 # TODO
-# class Test__init__:
-# class Test_fasta_file_property:
 # class Test_writefasta:
+    # write FastaSequence
+    # write (header, sequence)
 # class Test_writefastas:
 # class Test__repr__:
+
+
+
+
+# tested in Test__Init__:
+#   class Test_fasta_file_property
